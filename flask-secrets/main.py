4e244a18ea5df6598b3208e68c9591b5fae28af7
Szabo Gergo <szabo.gergo.bme@gmail.com>
2023-07-19 15:42:32 +0200

    bootstrap initialized in main, load css at htmls

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
@@ -12,6 +13,7 @@ class LoginForm(FlaskForm):
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap4(app)
 app.secret_key = os.environ['SECRET']
 
 
